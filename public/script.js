53c634c7d373a0260fafaf7aa6de55b5a2e629c5
Megan Ford <meg.c.ford@gmail.com>
2016-05-26 14:44:58 -0500

    added login page. edit button works.

--- public/script.js
+++ public/script.js
@@ -1,17 +1,33 @@
+/*eslint-disable no-console */
+/*global firebase:false*/
+
 $(() =>{ //jquery ready function.
-  console.log("DOM READY!");
-//the same as: $.ready(function() {
-// console.log("DOM READY!");
-// })
+  // console.log("DOM READY!");
+  //the same as: $.ready(function() {
+  // console.log("DOM READY!");
+  // })
 
-//ok so first I went onto firebase and made a new project. then I can add the name of the super-object (in this case task) to the end of the URL. took the .json out, since I needed to add ID's to the end of this URL for the individual complete/delete buttons. 
+//ok so first I went onto firebase and made a new project. then I can add the name of the super-object (in this case task) to the end of the URL. took the ".json" off, since I needed to add ID's to the end of this URL for the individual complete/delete buttons. 
 
-const API_URL = "https://crud-nss-5-26-class-exercise.firebaseio.com/task"
+  const API_URL = "https://crud-nss-5-26-class-exercise.firebaseio.com/task";
 
 
+//we also had bower install firebase --save
 
-//READ: GET data from firebase and display in table.
-   $.get(`${API_URL}.json`)
+//under your project on firebase.com, 'add firebase to your web app' and it will auto-generate this object for you.  this initialization is an asynchronous call and runs automatically so will take time.
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyCzuVbza4DLN_2EoTcVKX6P142l7SGWLHA",
+    authDomain: "crud-nss-5-26-class-exercise.firebaseapp.com",
+    databaseURL: "https://crud-nss-5-26-class-exercise.firebaseio.com",
+    storageBucket: "crud-nss-5-26-class-exercise.appspot.com"
+  });
+
+//READ: GET data from firebase and display in table. Runs at start. Could technically also run at submit button if I assigned it a name, but see below for instead.
+
+  function loadTheThings () { 
+    $("tbody").text("");
+    $.get(`${API_URL}.json`)
       .done((data) => {
         console.log("data gotten from getter", data);
         //if the object doesn't exist, firebase will return null. ONE WAY:
@@ -23,60 +39,93 @@ const API_URL = "https://crud-nss-5-26-class-exercise.firebaseio.com/task"
           //append the data to the page. 
           Object.keys(data).forEach((id)=>{
             addItemToTable(data[id], id); //note I'm passing in the ID here so I can grab the individual message later. 
+            if (data[id].completed === true) {
+              console.log("hey this one is completed", id );
+              completeItem(id);
+            }
           //other option: for (key in data) {
           //  addItemToTable(data[key])
           //  }
-          })
-        };
-      //TODO: handle completed tasks. 
-   })
+          }); //otherwise don't do anything. 'null' is falsy.
+        }
+      });
+  }
+  loadTheThings();
 
-
- //CREATE: form submit event to POST data to firebase. 
-  $("form").submit(()=> {
+ //CREATE: form submit event to POST data to firebase. Here I am NOT reloading the entire database object, although it can be- I'm simply running the dom object with the ID that's returned by the POST Jquery method. 
+ 
+  $(".newTask").submit((e)=> {
+    //submit buttons refresh pages. make it stop.
+    e.preventDefault();
+    // grab the form text. 
+    const theText = $(".theTextBox").val();
     //by default, .post sends a form, which firebase doesn't like. We used json.stringify to make it work. ONE OPTION: 
     //$.post(`${API_URL}.json`, JSON.stringify({"task": "I was posted!"}))
-    
     //ANOTHER OPTION:  
     $.ajax({ //settings object
       url: `${API_URL}.json`, 
-      method: 'POST',
-      data: JSON.stringify({task: "WUT"})
-    })
-    //TODO: make this not refresh the page but still update the dom?  
-    //TODO: grab the form text. 
-  })
+      method: "POST",
+      data: JSON.stringify({task: `${theText}`}) //is going to be the text entered in the form. 
+    }).done((id) => { //the POST action returns the ID 
+      console.log("id from json", id );
+      loadTheThings();
+    });
+    $(".theTextBox").val("");
+  });
 
 
 
-//DELETE: click event on delete to send  DELETE to firebase
-$('tbody').on('click', ".delete", (e) =>{
-  const row = $(e.target).closest('tr'); 
-  const id = row.data('id')
+//DELETE: click event on delete to send DELETE to firebase
+  $("tbody").on("click", ".delete", (e) =>{ //not here we couldn't use $(".delete").click() because at the time of page load there were no delete-classed items to attach to.
+    const row = $(e.target).closest("tr"); 
+    const id = row.data("id");
   //debugger //we put a debugger here to check what the ID is. 
-  $.ajax({
-    url: `${API_URL}/${id}.json`,
-    method: 'DELETE'
-  }).done(() => {
-    row.remove();
-  })
-})
+    $.ajax({
+      url: `${API_URL}/${id}.json`,
+      method: "DELETE"
+    }).done(() => {
+      row.remove();
+    });
+  });
 
 
 
-//EDIT: click event on complete to send PUT/PATCH to firebase
+//EDIT: click event on complete to send PUT/PATCH to firebase. PUT replaces the contents of the object. Patch adds the data to the assigned object.
 
+  $("tbody").on("click", ".complete", (e) => {
+    const row = $(e.target).closest("tr");
+    const id = row.data("id");
 
-function addItemToTable (item, id) {
+    $.ajax({
+      url:`${API_URL}/${id}.json`,
+      method: "PATCH",
+      data: JSON.stringify({completed: true})
+    });
+    completeItem(id);
+  }); 
+
+  function completeItem (id) {
+  
+    const row = $("tbody").find(`tr[data-id = "${id}"]`);
+    console.log("row", row );
+    const textDiv = row.find($(".taskText"));
+    console.log("task text", textDiv);
+    const completeButtonToDisable = row.find($(".complete"));
+
+    $(textDiv).addClass("completed");
+    $(completeButtonToDisable).attr("disabled", "disabled");
+  }
+
+  function addItemToTable (text, id) {
     //best way to store the individual ID of the object from firebase (so you can edit or delete it later) will be in the HTML. use 'data' in the HTML to store the ID. start with data- . note that the ID is one level up from the main data object. make it second so if it doesn't have one (for some insane reason), the item will still show up. 
-  const row =`<tr data-id= ${id}>
-    <td>${item.task}</td>
+    const row =`<tr data-id = ${id}>
+    <td class="taskText">${text.task}</td>
     <td>
       <button class = "btn btn-success complete">Complete</button>
       <button class="btn btn-danger delete">Delete</button>
     </td>
-  </tr>`
-  $('tbody').append(row)
-}
+  </tr>`;
+    $("tbody").append(row);
+  }
 
 });//end of jquery ready function at top.
